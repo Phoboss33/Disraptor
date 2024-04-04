43039b1bbf8f9d29479a555449b9d5b8d7d63695
XEDDOM <gugl.osnova@gmail.com>
2024-04-04 20:45:35 +0500

    add map

--- static/js/map/script.js
+++ static/js/map/script.js
@@ -4,14 +4,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
     draggableElements.forEach(function (elem) {
         elem.addEventListener("mousedown", SelectItem, false);
-        //window.addEventListener("mouseup", mouseUp, false);
     });
     let selectedElement = null;
     let diffX = 0;
     let diffY = 0;
     let i = 1;
-    const rateX = 8;
-    const rateY = 229;
     const SizeSquare = 150;
 
     function SelectItem(e) {
@@ -33,29 +30,38 @@ document.addEventListener("DOMContentLoaded", function () {
         console.log("move");
         if (selectedElement) {
             selectedElement.style.position = "absolute";
-            //console.log(e.clientX);
-            //console.log(field.right);
-            //console.log(diffX);
-            //console.log(e.clientY);
-            //console.log(field.bottom);
-            //console.log(diffY);
             let text = document.getElementById("text"+ selectedElement.id.match(/\d+/)[0]);
-            //console.log(text.id);
-            //console.log(selectedElement.id.match(/\d+/)[0]);
             //selectedElement.style.left = (e.clientX > field.right ? field.right - SizeSquare : e.clientX < diffX ? diffX - SizeSquare : e.clientX - diffX) + "px";
+            //selectedElement.style.top = (e.clientY - window.scrollY > field.bottom ? field.bottom - SizeSquare : e.clientY - window.scrollY < diffY ? diffY - SizeSquare : e.clientY - diffY  + window.scrollY) + "px";
             selectedElement.style.left = (e.clientX - diffX + window.scrollX) + "px";
-            selectedElement.style.top = (e.clientY - window.scrollY > field.bottom ? field.bottom - SizeSquare : e.clientY - window.scrollY < diffY ? diffY - SizeSquare : e.clientY - diffY  + window.scrollY) + "px";
-            //selectedElement.style.top = (e.clientY - diffY + window.scrollY) + "px";
-            //console.log(window. pageYOffset);
-            //console.log(window. scrollY);
-            console.log(e.clientY - window.scrollY);
-            console.log(field.bottom);
+            selectedElement.style.top = (e.clientY - diffY + window.scrollY) + "px";
             if (e.clientY - diffY + window.scrollY >= field.top && e.clientX - diffX + window.scrollX >= field.left &&
-            selectedElement.getBoundingClientRect().bottom - window.scrollY <= field.bottom &&
-            selectedElement.getBoundingClientRect().right - window.scrollX <= field.right) {
-                text.value = `${e.clientX - diffX - rateX}, ${e.clientY - diffY - rateY}`;
+                selectedElement.getBoundingClientRect().bottom + window.scrollY <= field.bottom &&
+                selectedElement.getBoundingClientRect().right - window.scrollX <= field.right) {
+                let resX = selectedElement.getBoundingClientRect().left - field.left;
+                let resY = selectedElement.getBoundingClientRect().top - field.top;
+                text.value = `${Math.floor(resX)}, ${Math.floor(resY)}`;
             } else {
-                text.value = "Undefined";
+                console.log("Diselect");
+                window.removeEventListener("mousemove", mouseMove, false);
+                if (selectedElement.getBoundingClientRect().top < field.top) {
+                    selectedElement.style.top = field.top;
+                }
+                if (selectedElement.getBoundingClientRect().bottom > field.bottom) {
+                    selectedElement.style.bottom = field.bottom;
+                }
+                if (selectedElement.getBoundingClientRect().right > field.right) {
+                    selectedElement.style.right = field.right;
+                }
+                if (selectedElement.getBoundingClientRect().left < field.left) {
+                    selectedElement.style.left = field.left;
+                }
+                let resX = selectedElement.getBoundingClientRect().left - field.left;
+                let resY = selectedElement.getBoundingClientRect().top - field.top;
+                text.value = `${Math.floor(resX)}, ${Math.floor(resY)}`;
+                console.log(field.right);
+                console.log(selectedElement.getBoundingClientRect().right);
+                selectedElement = null;
             }
         }
 
